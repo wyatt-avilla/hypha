3593cd0425b3b69409c4f112b11416ef380a57a8
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-06-26 15:02:41 -0700

    clap

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+/// Simple systemd service monitor
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Space delimited service names to monitor (e.g syncthing.service)
+    #[arg(short, long, required = true, value_parser, num_args = 1.., value_delimiter = ' ')]
+    pub services: Vec<String>,
+
+    /// Port to run the server on
+    #[arg(short, long, default_value_t = 8910)]
+    port: u16,
+}
+
 fn main() {
     println!("Hello, world!");
 }
