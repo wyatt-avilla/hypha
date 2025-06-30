8cc351a428f4b7fb5c4da30d18d08dd327b0436e
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-06-30 00:39:08 -0700

    wifi connection

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,16 +1,29 @@
+use dotenvy_macro::dotenv;
 use embassy_executor::Spawner;
 use esp_idf_hal::{gpio::PinDriver, peripherals::Peripherals};
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
 
 mod blink;
+mod wifi;
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let _ = spawner;
     esp_idf_svc::sys::link_patches();
-    esp_idf_hal::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let peripherals = Peripherals::take().unwrap();
+    let mut peripherals = Peripherals::take().unwrap();
+    let sys_loop = EspSystemEventLoop::take().unwrap();
+    let nvs = EspDefaultNvsPartition::take().unwrap();
+
+    wifi::connect_to(
+        dotenv!("WIFI_SSID"),
+        dotenv!("WIFI_PASSWORD"),
+        &mut peripherals.modem,
+        sys_loop,
+        nvs,
+    )
+    .unwrap();
 
     let mut led = PinDriver::output(peripherals.pins.gpio5).unwrap();
 
---
+++ client/src/wifi.rs
@@ -0,0 +1,74 @@
+use esp_idf_hal::{modem::Modem, sys::EspError};
+use esp_idf_svc::{
+    eventloop::{EspEventLoop, System},
+    nvs::{EspNvsPartition, NvsDefault},
+    wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi},
+};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum WifiError {
+    #[error("Couldn't convert {0} into string")]
+    CredentialConversion(&'static str),
+
+    #[error("Couldn't initialize wifi driver ({0})")]
+    Driver(EspError),
+
+    #[error("Couldn't set wifi configuration ({0})")]
+    Configuration(EspError),
+
+    #[error("Couldn't start wifi ({0})")]
+    Start(EspError),
+
+    #[error("Couldn't connect to wifi ({0})")]
+    Connect(EspError),
+
+    #[error("Couldn't wait for network interface ({0})")]
+    NetworkInterfaceWait(EspError),
+}
+
+pub fn connect_to(
+    ssid: &'static str,
+    password: &'static str,
+    modem: &mut Modem,
+    sys_loop: EspEventLoop<System>,
+    nvs: EspNvsPartition<NvsDefault>,
+) -> Result<(), WifiError> {
+    log::info!("attempting async init");
+    log::info!("ssid: {ssid}");
+    log::info!("pass: {password}");
+    let mut wifi = BlockingWifi::wrap(
+        EspWifi::new(modem, sys_loop.clone(), Some(nvs)).map_err(WifiError::Driver)?,
+        sys_loop,
+    )
+    .map_err(WifiError::Driver)?;
+
+    log::info!("SETTING CONFIGUARTION");
+    let configuration = Configuration::Client(ClientConfiguration {
+        ssid: ssid
+            .try_into()
+            .map_err(|()| WifiError::CredentialConversion(ssid))?,
+        bssid: None,
+        auth_method: AuthMethod::WPA2Personal,
+        password: password
+            .try_into()
+            .map_err(|()| WifiError::CredentialConversion(ssid))?,
+        channel: None,
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&configuration)
+        .map_err(WifiError::Configuration)?;
+
+    wifi.start().map_err(WifiError::Start)?;
+    log::info!("Wifi started");
+
+    wifi.connect().map_err(WifiError::Connect)?;
+    log::info!("Wifi connected");
+
+    wifi.wait_netif_up()
+        .map_err(WifiError::NetworkInterfaceWait)?;
+    log::info!("Wifi netif up");
+
+    Ok(())
+}
